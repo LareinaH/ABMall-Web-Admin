263607598d2eae0676521c8f31b87fa32420506e
lquterqtd <378203086@qq.com>
2018-07-14 23:18:45 +0800

    add

--- src/models/indexAd.js
+++ src/models/indexAd.js
@@ -1,12 +1,19 @@
 import modelExtend from 'dva-model-extend';
-import picModel from './inviteQRcode';
-import { getPicList } from '../services/inviteQRcode';
+import { getPicList, saveAdUrl } from '../services/inviteQRcode';
+import { getGoodsListPage } from '../services/commodityAdd';
+import { getActivityListPage } from '../services/activityManage';
 
-export default modelExtend(picModel, {
+import commonModel from './common';
+
+const { pageModel } = commonModel;
+
+export default modelExtend(pageModel, {
   namespace: 'indexAd',
 
   state: {
     picList: [],
+    activityList: [],
+    commodityList: [],
   },
 
   subscriptions: {
@@ -15,6 +22,14 @@ export default modelExtend(picModel, {
         if (pathname === '/adManage/indexAd') {
           dispatch({
             type: 'getPicList',
+          });
+
+          dispatch({
+            type: 'getActivityList',
+          });
+
+          dispatch({
+            type: 'getCommodityList',
           });
         }
       });
@@ -34,7 +49,67 @@ export default modelExtend(picModel, {
         });
       }
     },
+    *getActivityList(_, { call, put }) {
+      const response = yield call(getActivityListPage, {
+        current: 1,
+        pageSize: 999,
+      });
+      if (response.code === 200) {
+        yield put({
+          type: 'setData',
+          payload: {
+            key: 'activityList',
+            value: response.data.list,
+          },
+        });
+      }
+    },
+    *getCommodityList(_, { call, put }) {
+      const response = yield call(getGoodsListPage, {
+        pageNum: 1,
+        pageSize: 999,
+        conditions: {
+          isOnSell: true,
+        },
+      });
+      if (response.code === 200) {
+        yield put({
+          type: 'setData',
+          payload: {
+            key: 'commodityList',
+            value: response.data.list,
+          },
+        });
+      }
+    },
+    *saveAdUrl({ payload }, { call, put, select }) {
+      const { id } = payload;
+      const { picList } = yield select(state => state.indexAd);
+      const adItem = picList.filter(x => x.id === id)[0];
+      const response = yield call(saveAdUrl, adItem);
+      if (response.code === 200) {
+        yield put({
+          type: 'showNotice',
+          payload: '保存成功',
+        });
+      } else {
+        yield put({
+          type: 'showNotice',
+          payload: `保存失败:${response.message}`,
+        });
+      }
+    },
   },
 
-  reducers: {},
+  reducers: {
+    saveAdUrlLocal(state, { payload }) {
+      const { picList } = state;
+      const { adItem, index } = payload;
+      picList[index] = adItem;
+      return {
+        ...state,
+        picList,
+      };
+    },
+  },
 });
--- src/routes/AdManage/IndexAd.js
+++ src/routes/AdManage/IndexAd.js
@@ -1,12 +1,124 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
+import { Radio, Select, Row, Col } from 'antd';
 import { connect } from 'dva';
 import NoticeModal from '../NoticeModal';
 import InviteQRcodeSingle from '../InviteQRcode/InviteQRcodeSingle';
+import FormLabel from '../../components/MyComponent/FormLabel';
+
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
 
 const IndexAd = ({ dispatch, loading, indexAd }) => {
-  const { picList } = indexAd;
-  const titleList = ['第一个广告图', '第二个广告图', '第三个广告图'];
+  const { picList, commodityList, activityList } = indexAd;
+
+  const getSelectContent = (adItem, index) => {
+    const type = adItem.linkType;
+    if (type === 'NOTHING') {
+      return null;
+    } else if (type === 'GOODS') {
+      return (
+        <Select
+          showSearch
+          style={{ width: '100%' }}
+          placeholder="请选择"
+          value={adItem.linkParams}
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+          onChange={value => {
+            const newAdItem = {
+              ...adItem,
+              linkParams: value,
+            };
+
+            dispatch({
+              type: 'indexAd/saveAdUrlLocal',
+              payload: {
+                adItem: newAdItem,
+                index,
+              },
+            });
+          }}
+        >
+          {commodityList.map(item => (
+            <Select.Option key={item.id.toString()}>{item.goodsName}</Select.Option>
+          ))}
+        </Select>
+      );
+    } else if (type === 'ACTIVIT') {
+      return (
+        <Select
+          showSearch
+          style={{ width: '100%' }}
+          placeholder="请选择"
+          value={adItem.linkParams}
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+          onChange={value => {
+            const newAdItem = {
+              ...adItem,
+              linkParams: value,
+            };
+
+            dispatch({
+              type: 'indexAd/saveAdUrlLocal',
+              payload: {
+                adItem: newAdItem,
+                index,
+              },
+            });
+          }}
+        >
+          {activityList.map(item => (
+            <Select.Option key={item.id.toString()}>{item.activityName}</Select.Option>
+          ))}
+        </Select>
+      );
+    }
+  };
+
+  const cardTitleContent = (titleText, adItem, index) => {
+    return (
+      <div>
+        <Row>
+          <Col span={24}>{titleText}</Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <FormLabel label="链接" />
+            <RadioGroup
+              value={adItem.linkType}
+              onChange={e => {
+                const newAdItem = {
+                  ...adItem,
+                  linkType: e.target.value,
+                };
+
+                dispatch({
+                  type: 'indexAd/saveAdUrlLocal',
+                  payload: {
+                    adItem: newAdItem,
+                    index,
+                  },
+                });
+              }}
+            >
+              <RadioButton value="NOTHING">不关联</RadioButton>
+              <RadioButton value="GOODS">商品</RadioButton>
+              <RadioButton value="ACTIVIT">活动</RadioButton>
+            </RadioGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>{getSelectContent(adItem, index)}</Col>
+        </Row>
+      </div>
+    );
+  };
 
   const getCardList = () => {
     return picList.map((adItem, index) => {
@@ -14,7 +126,7 @@ const IndexAd = ({ dispatch, loading, indexAd }) => {
         <InviteQRcodeSingle
           dispatch={dispatch}
           loading={loading}
-          title={titleList[index]}
+          title={cardTitleContent(`第${index + 1}个广告图`, adItem, index)}
           adItem={adItem}
           index={index}
           key={index}
